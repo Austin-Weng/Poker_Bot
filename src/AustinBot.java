8346e81ab74c2466fd22d7498ffd152b9693bdb4
Austin Weng <24aweng@athenian.org>
2023-12-01 08:36:52 -0800

    Changed main, added class for AustinBot

--- src/AustinBot.java
+++ src/AustinBot.java
@@ -1,2 +1,18 @@
+import java.util.HashSet;
+
 public class AustinBot {
+    private HashSet<Card> hand;
+    private int money = 0;
+    public AustinBot(int money) {this.money = money;}
+
+    public int getMoney(){
+        return money;
+    }
+    public void setMoney(int money){
+        this.money = money;
+    }
+
+    public HashSet<Card> getHand(){return hand;}
+
+    public void addCard(Card card){this.hand.add(card);}
 }
