5c8012b9443219ad416b61f80b7f7d50174a0d6a
harryzrh23 <25rzhang@athenian.org>
2023-12-01 08:49:30 -0800

    Bot

---
+++ src/HarryBot.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+public class HarryBot extends Player {
+    private HashSet<Card> hand;
+
+    public HarryBot(int money) {
+        this.setMoney(money);
+    }
+    public String decision(LinkedList<Card> cardsInPlay, int pot){
+        return "";
+    }
+
+}
