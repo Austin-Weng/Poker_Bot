c39c97c09034f6e1a03bd13ec6e9e7e1ec426494
Austin-Weng <24aweng@athenian.org>
2023-12-18 22:01:41 -0800

    wins class added

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,25 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        int austinBot1Wins = 0;
-        int harryBot1Wins = 0;
-        int austinBot2Wins = 0;
-        int harryBot2Wins = 0;
-        int austinBot3Wins = 0;
-        int harryBot3Wins = 0;
-        int austinBot4Wins = 0;
-        int harryBot4Wins = 0;
+        numWins austinBot1Wins = new numWins(0);
+        numWins harryBot1Wins = new numWins(0);
+        numWins austinBot2Wins = new numWins(0);
+        numWins harryBot2Wins = new numWins(0);
+        numWins austinBot3Wins = new numWins(0);
+        numWins harryBot3Wins = new numWins(0);
+        numWins austinBot4Wins = new numWins(0);
+        numWins harryBot4Wins = new numWins(0);
 
         for (int game = 1; game <= 10; game++) {
 
 
             AustinBot1 austinBot1 = new AustinBot1(300);
             HarryBot1 harryBot1 = new HarryBot1(300);
-            AustinBot2 austinBot2 = new AustinBot2(300);
+            AustinBot1 austinBot2 = new AustinBot1(300);
             HarryBot2 harryBot2 = new HarryBot2(300);
-            AustinBot3 austinBot3 = new AustinBot3(300);
+            AustinBot1 austinBot3 = new AustinBot1(300);
             HarryBot3 harryBot3 = new HarryBot3(300);
-            AustinBot4 austinBot4 = new AustinBot4(300);
+            AustinBot1 austinBot4 = new AustinBot1(300);
             HarryBot4 harryBot4 = new HarryBot4(300);
 
             Player[] orderOfPlay = {austinBot1, harryBot1, austinBot2, harryBot2, austinBot3, harryBot3, austinBot4, harryBot4};
@@ -36,21 +36,21 @@ public class Main {
         }
 
 
-        System.out.println("AustinBot1 Wins: " + austinBot1Wins);
-        System.out.println("HarryBot1 Wins: " + harryBot1Wins);
-        System.out.println("AustinBot2 Wins: " + austinBot2Wins);
-        System.out.println("HarryBot2 Wins: " + harryBot2Wins);
-        System.out.println("AustinBot3 Wins: " + austinBot3Wins);
-        System.out.println("HarryBot3 Wins: " + harryBot3Wins);
-        System.out.println("AustinBot4 Wins: " + austinBot4Wins);
-        System.out.println("HarryBot4 Wins: " + harryBot4Wins);
+        System.out.println("AustinBot1 Wins: " + austinBot1Wins.wins);
+        System.out.println("HarryBot1 Wins: " + harryBot1Wins.wins);
+        System.out.println("AustinBot2 Wins: " + austinBot2Wins.wins);
+        System.out.println("HarryBot2 Wins: " + harryBot2Wins.wins);
+        System.out.println("AustinBot3 Wins: " + austinBot3Wins.wins);
+        System.out.println("HarryBot3 Wins: " + harryBot3Wins.wins);
+        System.out.println("AustinBot4 Wins: " + austinBot4Wins.wins);
+        System.out.println("HarryBot4 Wins: " + harryBot4Wins.wins);
     }
 
     private static void checkGameWinner(Player[] orderOfPlay, int game, Player austinBot1, Player harryBot1,
                                         Player austinBot2, Player harryBot2, Player austinBot3, Player harryBot3,
-                                        Player austinBot4, Player harryBot4, int austinBot1Wins, int harryBot1Wins,
-                                        int austinBot2Wins, int harryBot2Wins, int austinBot3Wins, int harryBot3Wins,
-                                        int austinBot4Wins, int harryBot4Wins) {
+                                        Player austinBot4, Player harryBot4, numWins austinBot1Wins, numWins harryBot1Wins,
+                                        numWins austinBot2Wins, numWins harryBot2Wins, numWins austinBot3Wins, numWins harryBot3Wins,
+                                        numWins austinBot4Wins, numWins harryBot4Wins) {
         Player winner = null;
         int maxMoney = 0;
 
@@ -68,24 +68,24 @@ public class Main {
             String winnerString = winner.toString();
 
             if (winnerString.contains("AustinBot1")) {
-                austinBot1Wins++;
+                austinBot1Wins.wins++;
             } else if (winnerString.contains("HarryBot1")) {
-                harryBot1Wins++;
+                harryBot1Wins.wins++;
             } else if (winnerString.contains("AustinBot2")) {
-                austinBot2Wins++;
+                austinBot2Wins.wins++;
             } else if (winnerString.contains("HarryBot2")) {
-                harryBot2Wins++;
+                harryBot2Wins.wins++;
             } else if (winnerString.contains("AustinBot3")) {
-                austinBot3Wins++;
+                austinBot3Wins.wins++;
             } else if (winnerString.contains("HarryBot3")) {
-                harryBot3Wins++;
+                harryBot3Wins.wins++;
             } else if (winnerString.contains("AustinBot4")) {
-                austinBot4Wins++;
+                austinBot4Wins.wins++;
             } else if (winnerString.contains("HarryBot4")) {
-                harryBot4Wins++;
+                harryBot4Wins.wins++;
             }
 
-            System.out.println("Game " + game + " Winner: " + winnerString);
+            //System.out.println("Game " + game + " Winner: " + winnerString);
         }
     }
 
---
+++ src/numWins.java
@@ -0,0 +1,4 @@
+public class numWins {
+    public int wins = 0;
+    public numWins(int wins){this.wins = wins;}
+}
